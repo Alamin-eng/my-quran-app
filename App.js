688d801e672516b22103737d55673dbc1c345195
Alamin-eng <alaminruben@gmail.com>
2026-07-23 00:22:29 +0100

    footer after each surah removed

--- App.js
+++ App.js
@@ -647,17 +647,8 @@ export default function App() {
           </View>
         ))}
 
-        {/* Footer Credit Layer */}
-        <View style={styles.footerDeveloperCredit}>
-          <Text
-            style={[styles.creditText, isDarkMode && styles.darkTextContent]}
-          >
-            Designed & Developed with ❤️ by Mohammad Al-Amin
-          </Text>
-          <Text style={styles.creditSubtext}>
-            May Allah reward your efforts. Ameen.
-          </Text>
-        </View>
+        /// Footer Credit component (Not in use but can be utilized directly in app.js by importing this component)
+
         <View style={{ height: 20 }} />
       </ScrollView>
     </SafeAreaView>
---
+++ footer.js
@@ -0,0 +1,12 @@
+{/* Not in use but can be utilized directly in app.js by importing this component   */}  
+{/* Footer Credit Layer */}
+        <View style={styles.footerDeveloperCredit}>
+          <Text
+            style={[styles.creditText, isDarkMode && styles.darkTextContent]}
+          >
+            Designed & Developed with ❤️ by Mohammad Al-Amin
+          </Text>
+          <Text style={styles.creditSubtext}>
+            May Allah reward your efforts. Ameen.
+          </Text>
+        </View>
